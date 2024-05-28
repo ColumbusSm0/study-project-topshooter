3e5c1290844c9bb6fd2c52d2f4c8450425c8452f
ColumbusSm0 <102983201+ColumbusSm0@users.noreply.github.com>
2024-05-28 11:44:13 -0300

    Initial setup of weapon ammo
    Implemented magazines and reload mechanics based on a maximum ammo pool.
    
    This feature is compatible with both weapons.
    
    TODO:
    
    Add reload animations
    Implement weapon-specific reload times and corresponding animations

--- Assets/Scripts/Characters/Player/Weapons/WeaponControl.cs
+++ Assets/Scripts/Characters/Player/Weapons/WeaponControl.cs
@@ -1,13 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
-using UnityEngine.Rendering.PostProcessing;
 using UnityEngine.VFX;
 
 public class WeaponControl : MonoBehaviour
 {
-    [Header("Game Objects and dependecies")]
-    // [SerializeField]private GameObject Bullet;
     [SerializeField] private GameObject BulletSpawn;
 
     public GameObject GunHolder;
@@ -16,56 +12,96 @@ public class WeaponControl : MonoBehaviour
     [SerializeField]private Animation InstanceMuzzleLight;
     public GunScriptableObject GunSO;
 
-    // [Header("Ammo and Shooting Stats")]
-    // [SerializeField]private bool isAutomatic;
+    
 
-    // [SerializeField]private int magazineSize;
+    [Header("Ammo and Shooting Stats")]
 
-    // [SerializeField]private int maxAmmo;
-
-    // private float timer;
-
-    // [Range(0.1f,1.5f)][SerializeField]private float delayBetweenShots;
+    public int CurrentMagazineBullets;
+    public int CurrentAmmo = 100;
 
     [Header("Sound Config")]
     public AudioSource WeaponAudioSource;
     [Range(0.1f, 0.5f)]
     public float pitchMultplier;
 
+    public GameObject BulletSpawn1 { get => BulletSpawn; set => BulletSpawn = value; }
 
-    // Start is called before the first frame update
     void Start()
     {
         GunInstance = UpdateEquippedWeapon(GunSO);
-        // SetGunEffects(GunInstance);
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (!GunSO.isAutomatic && Input.GetButtonDown("Fire1") && GunSO.timer > GunSO.delayBetweenShots)
         {
-            Shoot();
+            TryToShoot();
         }
 
         if (GunSO.isAutomatic && Input.GetButton("Fire1") && GunSO.timer > GunSO.delayBetweenShots)
         {
-            Shoot();
+            TryToShoot();
+        }
+
+        if ( Input.GetButtonDown("Reload"))
+        {
+            Reload();
+            Debug.Log("Tried Reload");
         }
 
         GunSO.timer += Time.deltaTime;
 
     }
 
-    void Shoot()
+    void TryToShoot()
     {
-        GunSO.timer = 0;
-        Instantiate(GunSO.Bullet, BulletSpawn.transform.position, BulletSpawn.transform.rotation);
-        WeaponAudioSource.pitch = Random.Range(1 - pitchMultplier, 1 + pitchMultplier);
-        WeaponAudioSource.PlayOneShot(GunSO.ShootSound);
-        InstanceMuzzleLight.Play();
-        InstanceMuzzleFlashEffect.Play();
+        if (CurrentMagazineBullets > 0)
+        {
+            GunSO.timer = 0;
+            Instantiate(GunSO.Bullet, BulletSpawn1.transform.position, BulletSpawn1.transform.rotation);
+            WeaponAudioSource.pitch = UnityEngine.Random.Range(1 - pitchMultplier, 1 + pitchMultplier);
+            WeaponAudioSource.PlayOneShot(GunSO.ShootSound);
+            InstanceMuzzleLight.Play();
+            InstanceMuzzleFlashEffect.Play();
+
+            CurrentMagazineBullets -= 1;
+        } else {
+
+            Reload();
+        }
         
+    }
+
+    public void Reload()
+    {
+        if(CurrentMagazineBullets < GunSO.magazineSize)
+        {
+            int bulletsToReload = GunSO.magazineSize - CurrentMagazineBullets;
+
+            if(CurrentAmmo > bulletsToReload)
+            {
+                CurrentMagazineBullets += bulletsToReload;
+                CurrentAmmo -= bulletsToReload;
+                Debug.Log("Reloaded");
+
+            }
+            else if (CurrentAmmo > 0)
+            {
+                CurrentMagazineBullets += CurrentAmmo;
+                CurrentAmmo = 0;
+                Debug.Log("Reloaded and out of ammo");
+            }
+            else
+            {
+                Debug.Log("No Ammo!");
+            }
+        }
+        else
+        {
+            Debug.Log("Magazine Full");
+            return;
+        }
+
     }
 
     public GameObject UpdateEquippedWeapon(GunScriptableObject newGun)
@@ -77,9 +113,15 @@ public class WeaponControl : MonoBehaviour
             GameObject.Destroy(g.transform.GetChild(i).gameObject);
         }
         GameObject GunInstance = Instantiate(newGun.GunPrefab,GunHolder.transform.position,GunHolder.transform.rotation,GunHolder.transform);
+        SetGunStats();
         SetGunEffects(GunInstance);
         return GunInstance;
 
+    }
+
+    private void SetGunStats()
+    {
+        CurrentMagazineBullets = GunSO.magazineSize;
     }
 
     public void SetGunEffects(GameObject GunInstance)
